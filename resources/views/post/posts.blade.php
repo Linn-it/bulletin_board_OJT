a79fd15777460b2b42204672d202dafb90486d2e
LinnAungHtet-MTM <136548606+LinnAungHtet-MTM@users.noreply.github.com>
2023-11-07 11:02:49 +0630

    fix search function

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 
 <div class="container mx-auto">
     <div class="flex justify-between mt-5 items-center flex-wrap mx-3">
+        @if (!str::contains(url()->current(),'my-post'))
         <form class="flex items-center" action="/posts">
             <div class="flex">
                 <div class="relative">
@@ -22,6 +23,24 @@ use Illuminate\Support\Str;
                 <a href="/posts" class="text-white mt-1.5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel</a>
             @endif
         </form>
+        @else
+        <form class="flex items-center" action="/posts/my-post">
+            <div class="flex">
+                <div class="relative">
+                    <div class="absolute inset-y-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                </div>
+                <input type="text" name="search" value="{{request('search')}}" class="bg-gray-50 border mr-2 border-gray-300 text-gray-900 text-sm rounded-lg block px-8  py-2">
+            </div>
+            <button type="submit" class="text-white mt-1.5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Search</button>
+            @if (request()->has('search'))
+                <a href="/posts/my-post" class="text-white mt-1.5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel</a>
+            @endif
+        </form>
+        @endif
 
         @if (!str::contains(url()->current(),'my-post'))
         <div>
